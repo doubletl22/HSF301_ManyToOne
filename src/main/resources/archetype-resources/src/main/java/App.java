436b4ad3617f792b89e69e480942a25279280f67
doubletl22 <kiyoshineko41@gmail.com>
2025-06-02 17:28:31 +0700

    manytoone

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $vn.edu.fu.hsf301;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
